5c5346a3830e4111b9882243d86771bc5fbcccc2
Vregis <vregis@mail.ru>
2018-08-21 14:24:15 +0300

    add articles

---
+++ frontend/controllers/ArticlesController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace frontend\controllers;
+
+
+use common\models\Article;
+use common\models\ArticleCategories;
+use yii\web\Controller;
+
+class ArticlesController extends Controller
+{
+    public function actionList($id = null)
+    {
+        $this->layout = false;
+        $articlesList = Article::find()->where(['public' => 1]);
+
+        if($id) {
+            $articlesList->andWhere(['category_id' => $id]);
+        }
+
+        $articlesList = $articlesList->all();
+        $categoriesList = ArticleCategories::find()->all();
+
+        return $this->render('categoriesList', ['articlesList' => $articlesList, 'categoriesList' => $categoriesList]);
+    }
+
+    public function actionIndex($alias)
+    {
+        $this->layout = false;
+        $article = Article::find()->where(['alias' => $alias])->one();
+        return $this->render('article', ['article' => $article]);
+    }
+}
